e4e90b156c3ab25d2474b7cab471de33fbc1db51
Rrossoni <57258482+Rrossoni@users.noreply.github.com>
2020-11-17 20:20:30 -0300

    Update 01-update.sh

--- 01-update.sh
+++ 01-update.sh
@@ -4,7 +4,6 @@ echo -e "Atualizando as listas do Apt"
     apt update
 
  echo -e "Atualizando os software"
-     
     apt -y upgrade
 
  echo -e "Forçando a atualização do sistema"
@@ -14,18 +13,14 @@ echo -e "Forçando a atualização de segurança"
     apt -y dist-upgrade
 
 echo -e "Removendo software desnecessários"   
-
     apt -y autoremove
     apt-y autoclean
     apt clean
 
-
 echo -e "Sistema Atualizado, pressione enter para o reboot"
    
      read
-
      sleep 5
-
      reboot
 
      
